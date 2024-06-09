31af7d7fe825e17861d1f1bed10b26ea17eb691f
Anton Marozau <AMarozau@gomel.iba.by>
2024-06-09 17:05:04 +0300

    Create GET /search Update HotelService Update HotelController

--- HotelApplication/src/main/java/com/demo/task/controller/HotelController.java
+++ HotelApplication/src/main/java/com/demo/task/controller/HotelController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.task.entity.Hotel;
@@ -27,5 +28,12 @@ public class HotelController {
 	@GetMapping("/{id}")
 	public ResponseEntity<Hotel> getHotelById(@PathVariable Long id) {
 		return hotelService.getHotelById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/search")
+	public List<Hotel> searchHotels(@RequestParam(required = false) String name,
+			@RequestParam(required = false) String brand, @RequestParam(required = false) String city,
+			@RequestParam(required = false) String county, @RequestParam(required = false) String amenities) {
+		return hotelService.searchHotels(name, brand, city, county, amenities);
 	}
 }
--- HotelApplication/src/main/java/com/demo/task/service/HotelService.java
+++ HotelApplication/src/main/java/com/demo/task/service/HotelService.java
@@ -2,6 +2,7 @@ package com.demo.task.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,17 +10,29 @@ import org.springframework.stereotype.Service;
 import com.demo.task.entity.Hotel;
 import com.demo.task.repository.HotelRepository;
 
+import io.micrometer.common.util.StringUtils;
+
 @Service
 public class HotelService {
 
-    @Autowired
-    private HotelRepository hotelRepository;
+	@Autowired
+	private HotelRepository hotelRepository;
 
-    public List<Hotel> getAllHotels() {
-        return hotelRepository.findAll();
-    }
+	public List<Hotel> getAllHotels() {
+		return hotelRepository.findAll();
+	}
 
-    public Optional<Hotel> getHotelById(Long id) {
-        return hotelRepository.findById(id);
-    }
+	public Optional<Hotel> getHotelById(Long id) {
+		return hotelRepository.findById(id);
+	}
+
+	public List<Hotel> searchHotels(String name, String brand, String city, String county, String amenities) {
+		return hotelRepository.findAll().stream()
+				.filter(hotel -> StringUtils.isEmpty(name) || hotel.getName().equalsIgnoreCase(name))
+				.filter(hotel -> StringUtils.isEmpty(brand) || hotel.getBrand().equalsIgnoreCase(brand))
+				.filter(hotel -> StringUtils.isEmpty(city) || hotel.getAddress().getCity().equalsIgnoreCase(city))
+				.filter(hotel -> StringUtils.isEmpty(county) || hotel.getAddress().getCounty().equalsIgnoreCase(county))
+				.filter(hotel -> StringUtils.isEmpty(amenities) || hotel.getAmenities().contains(amenities))
+				.collect(Collectors.toList());
+	}
 }
